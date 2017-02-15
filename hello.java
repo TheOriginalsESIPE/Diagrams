71c404531a7babea8a3df2e4bde0c259c92900db
YuxinSHI <yuxin.shi@etu.u-pec.fr>
2017-02-15 14:08:12 +0100

    demo

---
+++ hello.java
@@ -0,0 +1,12 @@
+/*************************************************************************
+	> File Name: hello.java
+	> Author: 
+	> Mail: 
+	> Created Time: mer. 15 févr. 2017 14:06:53 CET
+ ************************************************************************/
+
+public class Hello{
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+}
--- monFichier.md
+++
@@ -1,7 +0,0 @@
-je test le master commit
-
-j'ai aussi changer le fichier monFichier.md en ajoutant une ligne.
-
-Zadi change une ligne.
-
-
