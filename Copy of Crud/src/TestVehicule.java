aa48a327edda775e432e93add3050b038f973154
anais.hemici <anais.hemici@etu.u-pec.fr>
2017-02-17 13:31:55 +0100

    ajout CRUD

---
+++ Copy of Crud/src/TestVehicule.java
@@ -0,0 +1,25 @@
+import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.swing.SwingUtilities;
+
+public class TestVehicule {
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable(){
+		public void run(){
+			ModelVehicle m =new ModelVehicle();
+			ViewAuthentification v = new ViewAuthentification();
+			
+			//Controleur c = new Controleur(m, v);
+			ControllerAuthentification c = new ControllerAuthentification(m, v);
+			c.control();
+			
+		
+		}
+		});
+}
+}
