f784c977bb509a840b02c80a26553d8b6bf3b366
yuxin.shi <yuxin.shi@etu.u-pec.fr>
2017-03-27 09:30:27 +0200

    last change: complete to server

--- THE_ORIGINALS_PDS_2016-2017/src/dto/VehicleDTO.java
+++ THE_ORIGINALS_PDS_2016-2017/src/dto/VehicleDTO.java
@@ -2,7 +2,7 @@ package dto;
 
 public class VehicleDTO{
 	
-	private int numMat;
+	private String numMat;
 	private String model;
 	private String mark;
 	private String vehicle_type;
@@ -14,17 +14,17 @@ public class VehicleDTO{
 		this.vehicle_type=vehicle_type;
 	}
 	
-	public VehicleDTO(int nM, String mo, String ma, String vt){
+	public VehicleDTO(String nM, String mo, String ma, String vt){
 		this.numMat=nM;
 		this.model=mo;
 		this.mark=ma;
 		this.vehicle_type=vt;
 	}
 	
-	public int getNumMat() {
+	public String getNumMat() {
 		return numMat;
 	}
-	public void setNumMat(int numMat) {
+	public void setNumMat(String numMat) {
 		this.numMat = numMat;
 	}
 	
--- THE_ORIGINALS_PDS_2016-2017/src/server/CPoolServHandler.java
+++ THE_ORIGINALS_PDS_2016-2017/src/server/CPoolServHandler.java
@@ -1,6 +1,15 @@
 package server;
 
+import dto.VehicleDTO;
+import enumeration.EnumOperation;
+import org.json.simple.parser.ParseException;
+import repository.ModelVehicle;
+import serialization.Deserialization;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.sql.SQLException;
 
 /**
  * Created by tearsyu on 15/03/17.
@@ -26,5 +35,30 @@ public class CPoolServHandler extends Thread {
 
     public void run(){
         server.msg.append("RUN...");
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            String jsonString = in.readLine();
+            Deserialization des = new Deserialization();
+
+            VehicleDTO vehicleDTO = des.deserialAVehicle(des.parseStringToJson(jsonString));
+            int action = des.deserialAction(des.parseStringToJson(jsonString));
+            /**
+             * Now we have action and vehicle, should call the Model
+             * */
+            ModelVehicle modelVehicle = new ModelVehicle();
+            if (action == EnumOperation.DELETE.getIndex()){
+                modelVehicle.delete(vehicleDTO.getNumMat());
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
