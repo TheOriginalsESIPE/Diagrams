1ff05510f05a1a6fab29792349ecbb8895599ba5
bakaryDembele <bakary.dembele@etu.u-pec.fr>
2017-06-04 18:35:31 +0200

    les classes et methodes de cas d'utilisation Enregistres vehicule

--- THE_ORIGINALS_PDS_2016-2017/src/serialization/DeserializationGson.java
+++ THE_ORIGINALS_PDS_2016-2017/src/serialization/DeserializationGson.java
@@ -2,10 +2,14 @@ package serialization;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+
+import dto.BreakdownDTO;
 import dto.IndicatorDTO;
+import dto.VehicleDTO;
 
 import java.lang.reflect.Type;
 import java.util.List;
+import java.util.Vector;
 
 /**
  * Created by tearsyu on 10/05/17.
@@ -19,5 +23,18 @@ public class DeserializationGson {
         Type type = new TypeToken<List<IndicatorDTO>>(){}.getType();
         List<IndicatorDTO> arrIndicator = g.fromJson(str, type);
         return arrIndicator;
+    }
+    
+   
+    public Vector<BreakdownDTO> deserialBreakdownDTO(String str){
+        Type type = new TypeToken<Vector<BreakdownDTO>>(){}.getType();
+        Vector<BreakdownDTO> breakdownList = g.fromJson(str, type);
+        return breakdownList;
+    }
+    
+    public Vector<VehicleDTO> deserialVehicleDTO(String str){
+        Type type = new TypeToken<Vector<VehicleDTO>>(){}.getType();
+        Vector<VehicleDTO> vehicleInfo = g.fromJson(str, type);
+        return vehicleInfo;
     }
 }
