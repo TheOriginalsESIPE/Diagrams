9c14971cb53f1fa486a3c012c7e98025ea22d304
lobiyed.karim <lobiyed.karim@gmail.com>
2017-06-10 07:44:54 +0200

    Add files via upload

--- THE_ORIGINALS_PDS_2016-2017/src/enumeration/EnumDTO.java
+++ THE_ORIGINALS_PDS_2016-2017/src/enumeration/EnumDTO.java
@@ -1,13 +1,10 @@
 package enumeration;
 
-/**
- * Created by tearsyu on 12/04/17.
- */
 public enum EnumDTO {
     ADMIN("AdministratorDTO",1), BREAKDOWN("BreakdownDTO", 2), DIRECTOR("DirectorDTO", 3), PARKING("ParkingDTO", 4),
     PIECE_COMSO("Piece_consumptionDTO", 5), PIECE_DETACHED("Piece_detachedDTO",6), PIECE_PURCH("Piece_purchaseDTO",6),
-    PIECE_STK("Piece_stockDTO", 7), REPAIRER("Repairer", 8), VEHICLE_WH("Vehicle_warehouse", 9), VEHICLE("VehicleDTO", 10),
-    WH("WarehouseDTO", 11), INDICATOR("Indicator", 12), VISITINGMOTIF("VisitingMotifDTO", 13),  OPERATION("OperationDTO", 13);
+    PIECE_STK("Piece_stockDTO", 7), REPAIRER("Repairer", 8), VEHICLE_WH("Vehicle_warehouse", 9), VEHICLE("Vehicle", 10),
+    WH("WarehouseDTO", 11);
 
     int i;
     String nameDTO;
@@ -26,3 +23,4 @@ public enum EnumDTO {
         return enumdto.i;
     }
 }
+
