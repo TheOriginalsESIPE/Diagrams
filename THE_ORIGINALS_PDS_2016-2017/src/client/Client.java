9904dd8332a62a55f279185da701390145c51de2
lobiyed.karim <lobiyed.karim@gmail.com>
2017-06-10 07:45:26 +0200

    Add files via upload

--- THE_ORIGINALS_PDS_2016-2017/src/client/Client.java
+++ THE_ORIGINALS_PDS_2016-2017/src/client/Client.java
@@ -1,20 +1,18 @@
-package client;
+package Client;
 
-import server.ControllerAuthentification;
-import view.ViewAuthentification;
-
-import javax.swing.*;
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.Socket;
 import java.util.Properties;
 
-/**
- *  Created by tearsyu on 15/03/17.
- *  This is client class, it will auto connect to the server when we launch the app.
- *  @author tearsyu
- */
-public class Client{
-    private Properties properties;
+import javax.swing.SwingUtilities;
+
+import View.Controleur;
+import View.View;
+
+
+public class Client {
+	private Properties properties;
     private InputStream in;
     private Socket client;
     private static String localhost;
@@ -30,20 +28,13 @@ public class Client{
     }
 
     public Socket getClient(){return client;}
-    /**
-     * Connect to Server in according to config file and close the file input stream.
-     * @throws IOException
-     * */
+   
     public boolean connectToServer() throws IOException {
         client = new Socket(localhost, port);
         closeStream();
         return  client.isConnected();
     }
 
-
-    /**
-     * Close the input stream.
-     */
     public void closeStream(){
         try {
             in.close();
@@ -52,9 +43,7 @@ public class Client{
         }
     }
 
-    /**
-     * Close the socket.
-     */
+
     public void closeSocket(){
         try {
             client.close();
@@ -63,32 +52,23 @@ public class Client{
         }
     }
 
+    
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
 
-            ViewAuthentification v = new ViewAuthentification();
+            View v = new View();
             System.out.println("Client view ");
             //Start the client and connect to the server.
             try {
                 Client client1 = new Client();
                 if(client1.connectToServer()){
                     System.out.println("The client connect to the server.");
-                    /*This controller doesn't pass the server, it connects directly to the server!!
-                    * I need an other type of serialization to send object!
-                    * */
-                    ControllerAuthentification c = new ControllerAuthentification(v, client1.getClient());
-                    c.control();
-
-                    //while(client.getClient().isConnected()){
-
-                    //}
-                } else {
-                    v.errorDialog(1);
-                    System.out.println("Can't not to connect to server.");
+                    
+                    Controleur c = new Controleur( v, client1.getClient());
+                    c.Control();
                 }
             } catch (IOException e) {
-                v.errorDialog(1);
                 e.printStackTrace();
             }
 
@@ -98,3 +78,4 @@ public class Client{
     }
 
 }
+
